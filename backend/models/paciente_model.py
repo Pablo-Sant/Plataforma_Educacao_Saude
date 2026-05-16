791a75ccfc0044f061c32387c738349c830d76a9
Pablo <pgssdesouza@gmail.com>
2026-05-16 17:33:30 -0300

    definindo paciente como profile

--- backend/models/paciente_model.py
+++ backend/models/paciente_model.py
@@ -1,21 +1,16 @@
-from sqlalchemy import Column, Integer, String, CheckConstraint, DateTime
+from sqlalchemy import Column, Integer, String, CheckConstraint, DateTime, ForeignKey
 from core.configs import DBBaseModel
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
 
 
-class PacienteModel(DBBaseModel):
+class PacienteProfile(DBBaseModel):
     __tablename__ = "pacientes"
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
+    id = Column(Integer, ForeignKey('users.id'))
     cpf = Column(String(11), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    senha_hash = Column(String, nullable=False)
-    telefone = Column(String(20), nullable=False)
     idade = Column(Integer, CheckConstraint('idade >= 0'), nullable=False)
-    data_criacao = Column(DateTime, default=datetime.now)
     
     atendimentos = relationship('AtendimentoModel', back_populates='paciente', cascade='all, delete-orphan')
     
