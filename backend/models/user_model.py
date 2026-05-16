e81ff06a6a9d74c17144c83860fb3cf9d14837fd
Pablo <pgssdesouza@gmail.com>
2026-05-16 17:32:49 -0300

    criação do model user

---
+++ backend/models/user_model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, CheckConstraint, DateTime
+from core.configs import DBBaseModel
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import Enum as SQLEnum
+
+class RoleEnum(str, Enum):
+    PACIENTE = 'paciente'
+    MEDICO = 'medico'
+
+
+
+class UserModel(DBBaseModel):
+    __tablename__='users'
+    
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=True)
+    telefone = Column(String(100), nullable=False)
+    cpf = Column(String(11), unique=True, nullable=False)
+    senha_hash = Column(String, nullable=False)
+    data_criacao = Column(DateTime, default=datetime.now)
+    role = Column(SQLEnum(RoleEnum), nullable=False)
+    
+    medico = relationship('MedicoProfile', back_populates='user', uselist=False)
+    paciente = relationship('PacienteProfile', back_populates='user', uselist=False)
+
