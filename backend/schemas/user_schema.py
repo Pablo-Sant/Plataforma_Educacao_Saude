0c8023c4b24f56d01db034c24e8bcfc7a1d40bf3
Pablo <pgssdesouza@gmail.com>
2026-05-22 17:35:47 -0300

    agregação de user, paciente e médico em um msm schema

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 from enum import Enum
 
 
-class UsuarioRoleEnum(str, Enum):
+class RoleEnum(str, Enum):
     PACIENTE = 'paciente'
     MEDICO = 'medico'
     
@@ -17,7 +17,9 @@ class UserBase(BaseModel):
     telefone: str = Field(..., min_length=5, max_length=20)
     cpf: str = Field(..., max_length=11)
     data_criacao: Optional[datetime] = None
-    role: UsuarioRoleEnum
+    role: RoleEnum
+    idade: Optional[int] = Field(None, ge=0, le=150)
+    crm: Optional[str] = Field(None, max_length=15)
     
     
 class UserInput(UserBase):
@@ -29,7 +31,7 @@ class UserUpdate(BaseModel):
     telefone: Optional[str] = Field(None, min_length=5, max_length=20)
     cpf: Optional[str] = Field(None, max_length=11)
     senha: Optional[str] = None
-    role: Optional[UsuarioRoleEnum]
+    role: Optional[RoleEnum]
     
     
 class UserResponse(UserBase):
